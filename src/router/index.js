c6fe479d22a566a222074fb956309ba26f4661b0
ychleo102615 <leo102615@gmail.com>
2025-08-07 00:14:59 +0800

    設定基本template

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/DashBoardView.vue'),
+    }
   ],
 })
 
---
+++ src/views/DashBoardView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>註冊</h1>
+        <input type="text" placeholder="Email" v-model="signUpData.email" />
+        <input type="text" placeholder="Password" v-model="signUpData.password" />
+        <input type="text" placeholder="Nickname" v-model="signUpData.nickname" />
+        <button type="button">SignUp</button>
+        <h1>登入</h1>
+        <input type="text" placeholder="Email" v-model="signInData.email" />
+        <input type="text" placeholder="Password" v-model="signInData.password" />
+        <button type="button">SignIn</button>
+        <h1>驗證</h1>
+        <button v-if="isLoggedIn" type="button">CheckOut</button>
+        <div v-else="isLoggedIn">尚未登入</div>
+        <h1>登出</h1>
+        <button v-if="isLoggedIn" type="button">SignOut</button>
+        <div v-else="isLoggedIn">尚未登入</div>
+
+    </div>
+</template>
+
+<script setup>
+
+import { ref, computed } from 'vue'
+
+const signUpData = ref({
+    email: '',
+    password: '',
+    nickname: ''
+});
+
+const signInData = ref({
+    email: '',
+    password: ''
+});
+
+const isLoggedIn = computed(() => {
+    // return !!localStorage.getItem('token');
+    return true;
+});
+
+</script>
+
+
+<style scoped></style>
